f460d06a0b2fe93635a181568e802a554e96d1aa
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-05-26 11:50:11 -0700

    remove unnessary code

--- userServer/userQueries.js
+++ userServer/userQueries.js
@@ -9,43 +9,8 @@ var connectionString = 'postgres://localhost:5432/';
 var db = pgp(connectionString);
 
 module.exports = {
-  // getAllPuppies: getAllPuppies,
-  // getSinglePuppy: getSinglePuppy,
   createUser: createUser,
-  // updatePuppy: updatePuppy,
-  // removePuppy: removePuppy
 };
-
-// function getAllPuppies(req, res, next) {
-//   db.any('select * from pups')
-//     .then(function(data) {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           data: data,
-//           message: 'Retrieved All puppies'
-//         });
-//     })
-//     .catch(function(err) {
-//       return next(err);
-//     })
-// }
-
-// function getSinglePuppy(req, res, next) {
-//   var pupID = parseInt(req.params.id);
-//   db.one('select * from pups where id = $1', pupID)
-//     .then(function(data) {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           data: data,
-//           message: 'got one puppy'
-//         });
-//     })
-//     .catch(function(err) {
-//       return next(err);
-//     });
-// }
 
 function createUser (req, res, next) {
   db.done('insert into user(username, password)' +
