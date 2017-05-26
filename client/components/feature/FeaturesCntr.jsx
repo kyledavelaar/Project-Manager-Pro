5a74dadf70506e734d8a6fe2375111c41f481e08
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-05-26 11:53:06 -0700

    remove old commented out code

--- client/components/feature/FeaturesCntr.jsx
+++ client/components/feature/FeaturesCntr.jsx
@@ -3,12 +3,6 @@ import Feature from './Feature.jsx';
 
 class FeaturesCntr extends Component {
   render() {
-    // console.log('FEATURES CONTROLLER', this.props.featuresArray)
-    // let featureList = [];
-    // for (let key in this.props.featuresArray) {
-    //   featureList.push(this.props.featuresArray[key])
-    // }
-    // console.log('FEATURE LIST REALLY ARRAY?', featureList);
     
     const featureArr = this.props.featuresArray.map((feature, index) => {
       //console.log('FEATURE', feature);
