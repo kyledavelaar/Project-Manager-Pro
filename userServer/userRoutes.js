7b997c80c5977618d9d96af8d987093a3c3cf545
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2017-05-26 11:50:31 -0700

    remove old routes

--- userServer/userRoutes.js
+++ userServer/userRoutes.js
@@ -8,10 +8,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/api/puppies', db.getAllPuppies);
-// router.get('/api/puppies/:id', db.getSinglePuppy);
+
 router.post('/createUser',(req,res,next) => {console.log('DB.CREATEUSER', db.createUser); next();} ,db.createUser);
-// router.put('/api/puppies/:id', db.updatePuppy);
-// router.delete('api/puppies/:id', db.removePuppy); 
+
 
 module.exports = router;
